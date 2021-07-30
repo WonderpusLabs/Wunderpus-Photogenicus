37e8c526cdaf43d3fb2f28a943453009ba9516bc
lolfill <lolfill0104@gmail.com>
2021-07-30 17:53:23 -0400

    YourFitnessPal base files

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './client/index.js',
+	mode: process.env.NODE_ENV,
+	output: {
+		path: path.join(__dirname, '/build'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: path.join(__dirname, './client'),
+		publicPath: '/',
+		host: 'localhost',
+		port: 8080,
+		proxy: {
+			'/api': 'http://localhost:3000',
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
+				},
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+		  template: './client/index.html',
+		}),
+		new Dotenv()
+	  ],
+};
+
+
+

3f99ad9f70b1fe6902b1973bab982fb54468131d
lolfill <lolfill0104@gmail.com>
2021-07-31 14:56:05 -0400

    DB/Schema

--- server/models/models.js
+++ server/models/models.js
@@ -1,9 +1,6 @@
-// make sure to bcrypt password
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 const MONGO_URI = process.env.db_uri
-
 mongoose
 .connect(MONGO_URI, {
     // options for the connect method to parse the URI
@@ -11,9 +8,27 @@ mongoose
     useUnifiedTopology: true,
     // sets the name of the DB that our collections are part of
     dbName: 'Food',
-}) 
+})
 .then(() => console.log('Connected to Mongo DB.'))
 .catch((err) => console.log(err));
-
 const Schema = mongoose.Schema;
-
+const addMealSchema = new Schema({
+    name: {type: String, required: true},
+    foods:[foodSchema],
+    date: {type: Date, default: Date.now},
+});
+const addMeal = mongoose.model('meal', addMealSchema)
+const addFoodSchema = new Schema({
+    foodName: String,
+});
+const addFood = mongoose.model('food', addFoodSchema)
+const createUserSchema = new Schema ({
+    uid: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+})
+const createUser = mongoose.model('user', createUserSchema)
+module.exports = {
+    addMeal,
+    addFood,
+    createUser
+};

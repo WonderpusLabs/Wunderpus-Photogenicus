d6960142fae4090fbbecaf9d45bd28bee5e119ad
Phil Kang <83921181+philky-m@users.noreply.github.com>
2021-08-01 01:09:36 -0400

    Update cookieController.js

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -9,6 +9,7 @@ const cookieController = {};
 cookieController.setSSIDCookie = async (req, res, next) => {
   try {
     await User.findOne({username: res.locals.uid}, (req, user) => {
+      // create a cookie named 'ssid' with a value that is equal to the id of the user - mongoose creates an id for each user - method to get the id of the user 
       res.cookie('ssid', user._id.id, {httpOnly: true});
       return next();
     })

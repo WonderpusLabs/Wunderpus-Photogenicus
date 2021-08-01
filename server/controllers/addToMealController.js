2e2cf6f1b401ec5087b74b1f0a370d5a9e25cc66
Phil Kang <83921181+philky-m@users.noreply.github.com>
2021-08-01 00:57:44 -0400

    Update addToMealController.js

--- server/controllers/addToMealController.js
+++ server/controllers/addToMealController.js
@@ -3,6 +3,8 @@ const { model } = require('mongoose');
 const meals = require('../models/models.js');
 const addToMealController = {};
 
+
+// Phil comment - to find a meal within the meal model would we need to have a unique identifier for the meal?
 addToMealController.addtoMeal = async (req,res,next) => {
     model.Meal.find
 } 

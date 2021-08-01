9280ce90e0d78d1a1109ceb50a11e3f63eeada85
Phil Kang <83921181+philky-m@users.noreply.github.com>
2021-08-01 00:11:13 -0400

    Create userModel.js

---
+++ server/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+* Hint: Why is bcrypt required here?
+*/
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+});
+
+userSchema.pre('save', function (next) {
+  const salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
+  const hash = bcrypt.hashSync(this.password, salt);
+  this.password = hash;
+  return next();
+}) 
+
+module.exports = mongoose.model('User', userSchema);

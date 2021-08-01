00ced6a480f24fce9b4833a5f9badc71bc86fe52
Phil Kang <83921181+philky-m@users.noreply.github.com>
2021-08-01 00:06:49 -0400

    Update signupController.js

--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -1,13 +1,22 @@
 const path = require('path');
 const { model } = require('mongoose');
 
+const User = require('../models/userModel');
+
 const signupController = {};
 
-signupController.doSomething = (req, res, next) => {
-
+signupController.addNewUser = (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (username && password) {
+      res.locals.uid = username
+      return next();
+    }
+  } catch (err) {
+    return next({
+      log: 'error in the signupController addUser',
+      message: {err: 'Error occured in signupController addUser'}
+    })
+  }
 }
-
-// add new user to database
-// bcrypt password
-
 module.exports = signupController;

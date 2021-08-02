a649c9ea628cff70a299a9c376588e82d3987842
Phil Kang <m.philipkang@gmail.com>
2021-08-02 15:13:32 -0400

    Phil's update to api.js and searchController

--- server/routers/api.js
+++ server/routers/api.js
@@ -4,15 +4,20 @@ const router = express.Router();
 const signupController = require('../controllers/signupController')
 const cookieController = require('../controllers/cookieController')
 const loginController = require('../controllers/loginController')
+const searchController = require('../controllers/loginController')
 
 
 router.post('/signup', signupController.addNewUser, cookieController.setSSIDCookie, (req, res) => {
-  res.sendStatus(200);
+  return res.sendStatus(200);
 });
 
 
 router.post('/login', loginController.verifyUser, cookieController.setSSIDCookie, (req, res) => {
-  res.sendStatus(200);
+  return res.sendStatus(200);
+});
+
+router.post('/search', searchController.searchFood, (req, res) => {
+  return res.status(200).json(res.locals.foodSearch);
 });
 
 module.exports = router;
